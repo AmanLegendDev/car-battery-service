bb16afa2f74bed1c0d647e73da975f368ff83ca5
AmanLegendDev <amanansaricodes@gmail.com>
2026-09-25 17:09:33 +0530

    final push

--- components/services/detail/hero/serviceHeroData.ts
+++ components/services/detail/hero/serviceHeroData.ts
@@ -2,4 +2,4 @@ export const SERVICE_HERO_LABEL = "Mobile Car Battery Service";
 
 export const SERVICE_HERO_DEFAULT_CTA = "Book a Service";
 
-export const SERVICE_HERO_SECONDARY_CTA = "Call Now";
+export const SERVICE_HERO_SECONDARY_CTA = "Call Now +61 467 037 886";
